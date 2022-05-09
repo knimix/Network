ed17ce551aab5ed1ded4a0b71bb8a07b3133bb30
Konrad <uFQrkZgsGpwSU2B4skxG>
2022-05-09 11:01:14 +0200

    some fixes

--- export/Network/include/Network/Endpoint/Endpoint.h
+++ export/Network/include/Network/Endpoint/Endpoint.h
@@ -8,7 +8,7 @@ class sockaddr_in;
 namespace Network{
     class Endpoint {
     public:
-        Endpoint(){ m_IP.resize(16);m_IPBytes.resize(sizeof(uint32_t));}
+        Endpoint() = default;
         explicit Endpoint(sockaddr* address);
         Endpoint(const char* ip, unsigned  short port);
         inline const std::string& GetHostname() const {return m_Hostname;}
@@ -16,7 +16,9 @@ namespace Network{
         inline const std::vector<uint8_t>& GetIPBytes() const {return m_IPBytes;}
         inline const unsigned short GetPort() const {return m_Port;}
         sockaddr_in GetSockAddress() const;
+        explicit operator bool() const{return m_Success;}
     private:
+        bool m_Success = false;
         std::string m_Hostname;
         std::string m_IP;
         std::vector<uint8_t> m_IPBytes;
