93b52d19b525d977db7fc05ced0c231cdb5da737
konrad <knimixyt@gmail.com>
2023-04-10 21:52:21 +0200

    bug fixes

--- src/Socket/RawSocket.cpp
+++ src/Socket/RawSocket.cpp
@@ -30,7 +30,7 @@ namespace Network {
 #ifdef WINDOWS
         unsigned long mode = blocking ? 0 : 1;
         return (ioctlsocket(mHandle, FIONBIO, &mode) == 0);
-#elifdef LINUX
+#else
         int flags = fcntl(mHandle, F_GETFL, 0);
         if (flags == -1) return false;
         flags = blocking ? (flags & ~O_NONBLOCK) : (flags | O_NONBLOCK);
